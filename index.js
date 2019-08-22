e32e2a770ab998cdb47ca6ea4622bf69f5ff72b8
mulondo26 <mulondo26@gmail.com>
2019-08-22 16:44:29 +0300

    ft(authentication): user login - create the login route - create a passport script to configure the strategy [start #166812299]

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const config = require('./config/config');
-const users = require('./routes/users')
+const users = require('./routes/users');
+const passport = require('passport');
 // configure mongo db
 const dbUrl = config.mongodbUrl
 mongoose.connect(dbUrl)
   .then(()=> console.log('MongDB successfully connected'))
-  .catch(err => console.log(err));
+  .catch(err => console.log('========>',err));
 
 // configure .env 
 dotenv.config()
@@ -16,14 +18,17 @@ dotenv.config()
 const app = express()
 
 app.use(bodyParser.json());
+app.use(cors())
 app.use(bodyParser.urlencoded({extended: false}))
+app.use(passport.initialize())
+require('./config/passport')(passport)
 
 app.use('/api/auth/', users)
 
-const port = process.env.PORT || 3200
+const port = process.env.PORT || 3400
 
 app.listen(port, ()=> {
     console.log('running on ', port)
-})
+}) 
 
 module.exports = app;
