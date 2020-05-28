f3dcdf3c4917d4a77757b29a2fb479262c948f9b
mulondo <mulondo26@gmail.com>
2020-05-29 01:36:13 +0300

    add post experience

---
+++ validation/experienceValidation.js
@@ -0,0 +1,23 @@
+const Validator = require('validator');
+const isEmpty = require('./is-empty');
+
+module.exports = function validateExperienceInput(data) {
+    let errors = {}
+
+    title  = isEmpty(data.title)? '': data.title
+    from = isEmpty(data.from)? '': data.from
+    
+
+    if (Validator.isEmpty(title)){
+        errors.title = 'job title is missing';
+    }
+    if (Validator.isEmpty(from)){
+        errors.from = 'from date is missing';
+    }
+    
+    
+    return {
+        errors,
+        isValid: isEmpty(errors)
+    }
+}
