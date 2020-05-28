19487d2ad7679cac79f78b9718d3f36e420a3949
mulondo <mulondo26@gmail.com>
2020-05-29 01:36:13 +0300

    add post experience

--- validation/userProfileValidation.js
+++ validation/userProfileValidation.js
@@ -1,50 +1,45 @@
 const Validator = require('validator');
 const isEmpty = require('./is-empty');
 
-module.exports = function validateBlogInput(data) {
+module.exports = function validateProfileInput(data) {
     let errors = {}
    
-  
-    // more work is still required here
-    data.text = !isEmpty(data.text) ? data.text: '';
     
-    // data.category = typeof(data.category) ==='undefined'? '': data.category
-
-    inspiration = isEmpty(data.inspiration)? '': data.inspiration
     handle  = isEmpty(data.handle)? '': data.handle
+    inspiration = isEmpty(data.inspiration)? '': data.inspiration
     hobbies = isEmpty(data.hobbies)? '': data.hobbies
     bio = isEmpty(data.bio)? '': data.bio
     status = isEmpty(data.status)? '': data.status
     company = isEmpty(data.company)? '': data.company
     
 
-    if (!Validator.isLength(data.handle, {min:2, max:80})){
+    if (!Validator.isLength(handle, {min:2, max:80})){
         errors.handle = 'handle must be at least 2 or more characters';
     }
 
-    if (Validator.isEmpty(data.handle)){
-        errors.handle = 'handle field is missing';
+    if (Validator.isEmpty(handle)){
+        errors.handle = 'profile handle is missing';
     }
 
     if(!isEmpty(data.facebook)){
-        if(!Validator.isURL(data.facebook)){
+        if(!Validator.isURL(facebook)){
             errors.facebook = 'Url is invalid'
         }
     }
 
     if(!isEmpty(data.youtube)){
-        if(!Validator.isURL(data.youtube)){
+        if(!Validator.isURL(youtube)){
             errors.youtube = 'Url is invalid'
         }
     }
 
     if(!isEmpty(data.twitter)){
-        if(!Validator.isURL(data.twitter)){
+        if(!Validator.isURL(twitter)){
             errors.twitter = 'Url is invalid'
         }
     }
     if(!isEmpty(data.instagram)){
-        if(!Validator.isURL(data.instagram)){
+        if(!Validator.isURL(instagram)){
             errors.instagram = 'Url is invalid'
         }
     }
