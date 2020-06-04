f0a08775096f339b4ca75a8eb3d63ecd6d13347a
mulondo <mulondo26@gmail.com>
2020-06-04 21:33:57 +0300

    configure cors in headers

--- index.js
+++ index.js
@@ -20,6 +20,11 @@ mongoose.connect(dbUrl)
 dotenv.config()
 
 const app = express()
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(bodyParser.json());
 app.use(cors())
